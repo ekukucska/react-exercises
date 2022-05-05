5702699874388d4924ac6c2abb858eae5b58b93e
ekukucska <andreikukucska@gmail.com>
2022-05-06 00:34:21 +0300

    [RE-13]: Added useContext, minor modifications in routes

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,6 +5,7 @@ const HomePage = () => {
     <>
       <h1>Home Page</h1>
       <ul><h3>Topics:</h3></ul>
+      <li><Link to="/react-hooks">React Hooks Page</Link></li>
       <li><Link to="/env-variables">Env Variables Page</Link></li>
       <li><Link to="/node-api">Node API Response Page</Link></li>
       <li><Link to="/error-boundary">Error Boundary Page</Link></li>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,25 +1,38 @@
+import React, { useState, useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ErrorBoundary from "../components/ErrorBoundary";
 import Layout from "../components/Layout";
+import ThemeContext from "../context/ThemeContext";
 import HomePage from "../pages/HomePage";
 import EnvVariablesPage from "../pages/EnvVariablesPage";
 import NodeAPIPage from "../pages/NodeAPIPage";
 import ErrorBoundaryPage from "../pages/ErrorBoundaryPage";
+import ReactHooksPage from "../pages/ReactHooksPage";
+import UseContextPage from "../pages/UseContextPage";
 import NoPage from "../pages/NoPage";
-import ErrorBoundary from "../components/ErrorBoundary";
 
 const RouterRoutes = () => {
+  const [theme, setTheme] = useState("theme-light");
   return (
     <ErrorBoundary>
       <Router>
+        <ThemeContext.Provider value={[theme, setTheme]} >
         <Routes>
-          <Route path={'/' || '/home'} element={<Layout />}>
+          <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
             <Route path="env-variables" element={<EnvVariablesPage />} />
             <Route path="node-api" element={<NodeAPIPage />} />
             <Route path="error-boundary" element={<ErrorBoundaryPage />} />
-            <Route path="*" element={<NoPage />} />
           </Route>
+          <Route path="react-hooks" element={<Layout />}>
+            <Route index element={<ReactHooksPage />} />
+            <Route path=":react-hook" element={<UseContextPage />} />
+            <Route path="usecontext-hook" element={<UseContextPage />} />
+          </Route>
+          <Route path="*" element={<NoPage />} />
         </Routes>
+        </ThemeContext.Provider>
       </Router >
     </ErrorBoundary>
   );
