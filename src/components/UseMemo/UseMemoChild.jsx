80178decf6378e7a17398c9d757003169566f36c
ekukucska <andreikukucska@gmail.com>
2022-06-12 22:02:08 +0300

    [RE-18]: Added useMemo page

---
+++ src/components/UseMemo/UseMemoChild.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const UseMemoChild = () => {
+  console.log('useMemo Child component is rendering');
+
+  return (
+    <div>
+      <p>
+        Child
+      </p>
+    </div>
+  )
+}
+
+export default UseMemoChild;
---
+++ src/pages/UseMemoPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import UseMemo from '../../components/UseMemo';
+
+
+const UseMemoPage = () => {
+  return (
+    <>
+      <h1>useMemo Hook</h1>
+      <h3>useMemo:</h3>
+      <br />
+      <UseMemo />
+    </>
+  );
+};
+
+export default UseMemoPage;
