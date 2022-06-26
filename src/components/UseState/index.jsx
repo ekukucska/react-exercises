b7cbf9fa9a59444834faefeac3d434fd1d65805a
ekukucska <andreikukucska@gmail.com>
2022-06-26 23:20:51 +0300

    [RE-21]: useState with object or array values

--- src/components/UseState/index.jsx
+++ src/components/UseState/index.jsx
@@ -7,6 +7,8 @@ import React, { useState } from "react";
 
 const UseState = () => {
   const [count, setCount] = useState(0);
+  const [user, setUser] = useState({});
+  const [list, setList] = useState([]);
 
   const increaseCount = () => {
     setCount(prevCount => prevCount + 1);
@@ -18,12 +20,45 @@ const UseState = () => {
     setCount(0);
   }
 
+  const userJohn = () => {
+    setUser({ name: 'John', age: 34 });
+  }
+  const userBill = () => {
+    setUser({ name: 'Bill', age: 49 });
+  }
+
+  const toDoList = () => {
+    setList(['buy groceries', 'cook lunch', 'vacuum', 'workout']);
+  }
+  const shoppingList = () => {
+    setList(['bread', 'apples', 'milk', 'chicken', 'eggs', 'oranges']);
+  }
+
   return (
     <>
+      <h4>useState - passing a function:</h4>
+      <div>If the new state is computed using the previous state, you can pass a function to setState.</div>
+      <br />
       <div>Counter: {count}</div>
+      <br />
       <button onClick={increaseCount}>Icrease count</button>
       <button onClick={resetCount}>Reset count</button>
       <button onClick={decreaseCount}>Decrease count</button>
+      <h4>useState - with an object value:</h4>
+      <div>User: {user.name}</div>
+      <div>Age: {user.age}</div>
+      <button onClick={userJohn}>John</button>
+      <button onClick={userBill}>Bill</button>
+      <br />
+      <h4>useState - with an array value:</h4>
+      <button onClick={toDoList}>To Do List</button>
+      <button onClick={shoppingList}>Shopping List</button>
+      <p>List:</p>
+      <ul>
+        {list.map((listItem) => (
+          <li key={listItem}>{listItem}</li>)
+        )}
+      </ul>
     </>
   );
 };
