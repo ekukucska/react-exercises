5e0352e3c178f364b956eeee87b2a32195bc3e54
ekukucska <andreikukucska@gmail.com>
2022-06-28 00:43:48 +0300

    [RE-22]: useEffect with cleanup + some constants modifications

---
+++ src/constants/URLs/index.js
@@ -0,0 +1,1 @@
+export const JSON_PLACEHOLDER_POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
--- src/pages/ReactHooksPage/index.js
+++ src/pages/ReactHooksPage/index.js
@@ -7,6 +7,7 @@ const ReactHooksPage = () => {
       <h1>React Hooks Page</h1>
       <ul><h3>Featured Hooks:</h3></ul>
       <li><Link to="/react-hooks/usestate-hook">useState Hook</Link></li>
+      <li><Link to="/react-hooks/useeffect-hook">useEffect Hook</Link></li>
       <li><Link to="/react-hooks/usecontext-hook">useContext Hook</Link></li>
       <li><Link to="/react-hooks/usememo-hook">useMemo Hook</Link></li>
       <li><Link to="/react-hooks/usecallback-hook">useCallback Hook</Link></li>
